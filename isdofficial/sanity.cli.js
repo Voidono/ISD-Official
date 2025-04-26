6e333ccdac476f4e1e456343379388e06c303816
manh1122005 <voidoistaken@gmail.com>
2025-04-26 13:13:21 +0700

    gfbdg

--- isdofficial/sanity.cli.js
+++ isdofficial/sanity.cli.js
@@ -5,6 +5,9 @@ export default defineCliConfig({
     projectId: 'p9o776pf',
     dataset: 'isd-cms'
   },
+  project: {
+    basePath: '/studio'
+  },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
